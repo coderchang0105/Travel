647e3f6faea350dc39c2a1aed9303931d9204aa8
coderchang <coderchang0105@gmail.com>
2018-06-01 18:57:17 +0800

    city-ajax

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list">
+    <div class="item" v-for="(item, key) of cities" :key="key">{{key}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    right: 0
+    top: 1.58rem
+    bottom: 0
+    width: .4rem
+    .item
+      text-align: center
+      line-height: .4rem
+      color: $bgColor
+</style>
