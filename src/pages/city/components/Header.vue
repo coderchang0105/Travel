7463478ebaf7ee63384377bcff9c8b9076f5a9df
coderchang <coderchang0105@gmail.com>
2018-06-01 16:32:29 +0800

    city-header finished

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    position: relative
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    overflow: hidden
+    background: $bgColor
+    .header-back
+      text-align: center
+      font-size: .4rem
+      position: absolute
+      left: .1rem
+      top: 0
+      color: #fff
+</style>
