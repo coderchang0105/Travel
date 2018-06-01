0d68e84ce37feed5531ab07c5acc53e973447f97
coderchang <coderchang0105@gmail.com>
2018-06-01 16:01:42 +0800

    index finished

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom" v-for="recommend of recommendList" :key="recommend.id">
+      <li class="item border-bottom" v-for="recommend of list" :key="recommend.id">
         <img class="item-img" :src="recommend.imgUrl">
         <div class="item-info">
           <p class="item-info-title">{{recommend.title}}</p>
@@ -17,35 +17,8 @@
 <script>
 export default {
   name: 'HomeRecommend',
-  data () {
-    return {
-      recommendList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1803/7b/7b023b9728f0834fa3.img.jpg_200x200_5fddc1d8.jpg',
-        title: '世界花卉大观园',
-        desc: '揽天下奇花异草，聚世界经典园林'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/201308/08/41150dacc0e3a0c8c8d65eac.jpg_200x200_c62292e9.jpg',
-        title: '北京动物园',
-        desc: '百余种动物，让你离自然更近'
-      }, {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_200x200_8eb33f07.jpg',
-        title: '恭王府',
-        desc: '一起去看看和珅家'
-      }, {
-        id: '0004',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1601/f5/f5069b6e5f9488b590.water.jpg_200x200_587c485c.jpg',
-        title: '德云社相声大会广德楼店',
-        desc: '赏德云社爆笑演出，High翻全场乐不停'
-      }, {
-        id: '0005',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1608/4f/4fc3f5a1b838f279a3.water.jpg_200x200_e6528ef6.jpg',
-        title: '北京杜莎夫人蜡像馆',
-        desc: '中轴线上的巨星之旅'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <swiper :options="swiperOption" ref="mySwiper">
         <!-- slides -->
-        <swiper-slide v-for="item of swiperList" :key="item.id">
+        <swiper-slide v-for="item of list" :key="item.id">
           <img class="swiper-img" :src="item.imgUrl">
         </swiper-slide>
         <!-- Optional controls -->
@@ -14,22 +14,15 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/b2/ea3845efcd579802.jpg_750x200_f2131e39.jpg'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
-      }, {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/a3/49267f3b84733902.jpg_750x200_0f6acf15.jpg'
-      }]
+      }
     }
   }
 }
