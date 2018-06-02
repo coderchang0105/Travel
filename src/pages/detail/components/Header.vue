19d0d5d6c4eb5cfc11788df6b3a7810c482eeace
coderchang <coderchang0105@gmail.com>
2018-06-02 16:40:40 +0800

    fix scroll problem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -26,6 +26,7 @@ export default {
   },
   methods: {
     handleScroll () {
+      console.log('scroll')
       const top = document.documentElement.scrollTop
       if (top > 60) {
         let opacity = top / 200
@@ -41,6 +42,9 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
